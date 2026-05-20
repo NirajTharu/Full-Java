9f1ea15782b21a5349cc788a9824c79b9d4e73eb
NirajTharu <nirajc279@gmail.com>
2026-05-20 21:44:11 +0545

    Adding the file of the string method in java

--- someStringMethods.java
+++ someStringMethods.java
@@ -11,10 +11,38 @@ public class someStringMethods {
         name = name.toLowerCase();
         System.out.printf("The lower case of the name is:%s\n",name);
         // name.trim() to trim the white space if any is present before or after the string
+        name = name.replace("a", "o");
+        System.out.println(name);
+        // This will return the boolean value
+        System.out.println("The string is empty: " + name);
         System.out.printf("The replace name is: %s\n",name);
         System.out.printf("The last index of r is at index of %d\n",lastIndex);
         System.out.printf("The index of the char i is in %d\n",index);
         System.out.printf("The first char in the name is %c\n",letter);
         System.out.printf("There are %d char in %s.\n",length, name);
+
+        if(name.isEmpty())
+        {
+            System.out.println("You name field is empty.");
+        }
+        else{
+            System.out.println("Welcome " + name);
+        }
+        if(name.contains("c"))
+        {
+            System.out.println("Your name contains: c .");
+        }
+        else{
+            System.out.println("Your name does not contains c.");
+        }
+
+        String address = "Bajar";
+        if(address.equals("Bajar"))
+        {
+            System.out.println("Your name is same.");
+        }
+        else{
+            System.out.println("Your name is unique.");
+        }
     }
 }
