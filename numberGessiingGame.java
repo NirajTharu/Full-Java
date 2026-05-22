90e86ef07159ee41066ae0892c810fa91d2aee2c
NirajTharu <nirajc279@gmail.com>
2026-05-22 22:45:13 +0545

    Adding the file of the number guession game

---
+++ numberGessiingGame.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class numberGessiingGame {
+   public static void main(String[] args) {
+    
+    // Number guessing game
+
+    Random random = new Random();
+    Scanner scanner = new Scanner(System.in);
+
+    int guess;
+
+   } 
+}
