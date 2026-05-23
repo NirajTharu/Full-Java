6fb7cb1352e737e92a6b32dd2858fbf97836db75
NirajTharu <nirajc279@gmail.com>
2026-05-23 09:19:27 +0545

    Adding file without any changes

--- numberGessiingGame.java
+++ numberGessiingGame.java
@@ -8,8 +8,31 @@ public class numberGessiingGame {
 
     Random random = new Random();
     Scanner scanner = new Scanner(System.in);
-
+    
     int guess;
+    int attempts = 0;
+    
+    int randomNumber = random.nextInt(1,101);
+    System.out.println("Number Guessing game: ");
+    System.out.println("Guess a number between 1 - 100: ");
+    do{
+      System.out.print("Enter a guess: ");
+      guess = scanner.nextInt();
+      attempts ++;
+      if(guess<randomNumber)
+      {
+         System.out.println("Guess a higher number.");
+      }
+      else if(guess > randomNumber)
+      {
+         System.out.println("Guess a lower number.");
+      }
+      else {
+         System.out.println("Correct the number was " + randomNumber);
+         System.out.println("# of attempts: " + attempts);
+      }
 
+    }while(guess != randomNumber);
+      scanner.close();
    } 
 }
