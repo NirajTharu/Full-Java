eff7cfe2251fe6ccacb9fd83d9a387b4d4daaa1d
NirajTharu <nirajc279@gmail.com>
2026-05-25 20:16:49 +0545

    Adding the file by modifying to the array

--- staticKeyWord.java
+++ staticKeyWord.java
@@ -5,14 +5,22 @@ public class staticKeyWord {
         // About static Key word:  Makes a variable or method belongs to the class rather than to any specific object.
         // Commonly used for utility methods or shared resources.
 
-        friends friend1 = new friends("Nikita");
-        friends friend2 = new friends("Alvaro");
-        friends friend3 = new friends("Hayato");
-        friends friend4 = new friends("Kelly");
-        friends friend5 = new friends("Maxim ");
-
+        // friends friend1 = new friends("Nikita");
+        // friends friend2 = new friends("Alvaro");
+        // friends friend3 = new friends("Hayato");
+        // friends friend4 = new friends("Kelly");
+        // friends friend5 = new friends("Maxim ");
 
         // System.out.println(friends.numOfFriends);
+
+
+        friends[] friendsList = 
+        {   new friends("Nikita"),
+            new friends( "alvaro"),
+            new friends("Hayato"),
+            new friends("Kelly"),
+            new friends("Maxim")
+        };
         friends.showFriends();  
     }
 }
